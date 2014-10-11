5d016ca30faf9e5a51a3da617aab1744a3cf744b
nature-sky <vbnwerbnm@gmail.com>
2014-10-11 22:19:14 +0800

    modified respond_to type. fix #1.

--- app/controllers/sikuli_controller.rb
+++ app/controllers/sikuli_controller.rb
@@ -1,7 +1,15 @@
 class SikuliController < ApplicationController
     def get_latest_date
+      @latest_date = File.new("app/assets/sikuli/test.txt.tar.bz2").mtime
+      respond_to do |format|
+        format.any { render :text => @latest_date }
+      end
     end
 
     def get_latest_script
+      @latest_script = File.new("app/assets/sikuli/test.txt.tar.bz2")
+      respond_to do |format|
+        format.zip { send_file @latest_script }
+      end
     end
 end
